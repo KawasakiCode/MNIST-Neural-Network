33debf29728bb7bbb2796bc2127f94e19e281068
Kawasaki <remvosp@gmail.com>
2026-04-06 14:25:37 +0300

    finished optimizer and gradient descent and made the training loop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data import load_and_prep_data
 from network import initialize_weights_biases, output_layer_derivatives, hidden_layer_derivatives, input_layer_derivatives
 from activations import ReLU, Softmax
 from losses import Categorical_Cross_Entropy
+from optimizers import gradient_descent
 
 TRAIN_FILEPATH = "mnist_train/mnist_train.csv"
 TEST_FILEPATH = "mnist_test/mnist_test.csv"
@@ -12,28 +13,35 @@ X_test, Y_test = load_and_prep_data(TEST_FILEPATH)
 
 W1, W2, b1, b2 = initialize_weights_biases(128)
 
-# The intermediate step of the hidden layer which is passed through ReLU
-Z1 = X_train @ W1 + b1
+# Data to monitor training
+loss_history = []
+accuracy_history = []
 
-# The output of the hidden layer
-A1 = ReLU(Z1)
+for epoch in range(1000):
+    # The intermediate step of the hidden layer which is passed through ReLU
+    Z1 = X_train @ W1 + b1
 
-# Pass the output of the hidden layer into the output layer
-Z2 = A1 @ W2 + b2
+    # The output of the hidden layer
+    A1 = ReLU(Z1)
 
-# Final output of the network
-A2 = Softmax(Z2)
+    # Pass the output of the hidden layer into the output layer
+    Z2 = A1 @ W2 + b2
 
-# Calculate loss
-CCE_loss = Categorical_Cross_Entropy(A2, Y_train)
+    # Final output of the network
+    A2 = Softmax(Z2)
 
-# Calculate derivatives with Chain Rule
-dZ2 = output_layer_derivatives(A2, Y_train)
-dW2, db2 = hidden_layer_derivatives(A1, dZ2)
-dW1, db1 = input_layer_derivatives(dZ2, W2, Z1, X_train)
+    # Calculate loss
+    CCE_loss = Categorical_Cross_Entropy(A2, Y_train)
+    loss_history.append(CCE_loss)
 
-# Send to optimizer to apply gradient descent
+    # Calculate derivatives with Chain Rule
+    dZ2 = output_layer_derivatives(A2, Y_train)
+    dW2, db2 = hidden_layer_derivatives(A1, dZ2)
+    dW1, db1 = input_layer_derivatives(dZ2, W2, Z1, X_train)
 
+    # Send to optimizer to apply gradient descent
+    lr = 0.01
+    W1, W2, b1, b2 = gradient_descent(dW1, dW2, db1, db2, W1, W2, b1, b2, lr)
 
 
 
---
+++ optimizers.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def gradient_descent(dW1: np.ndarray, dW2: np.ndarray, db1: np.ndarray, db2: np.ndarray, W1: np.ndarray, W2: np.ndarray, b1: np.ndarray, b2: np.ndarray, learning_rate: float):
+    # Calculate new weights
+    W1_new = W1 - (learning_rate * dW1)
+    W2_new = W2 - (learning_rate * dW2)
+
+    # Calculate new biases
+    b1_new = b1 - (learning_rate * db1)
+    b2_new = b2 - (learning_rate * db2)
+
+    return W1_new, W2_new, b1_new, b2_new
