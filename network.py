e0ba7f19dc3f38665725a964a3a5f5c5bb4c4d25
Kawasaki <remvosp@gmail.com>
2026-04-11 17:59:59 +0300

    changed the architecture of the train file by passing all forward and backward functions into separate files and renaming all the variables for clarity and to make it easy to add more layers later. Finished forward pass of convolutional layer

--- network.py
+++ network.py
@@ -5,10 +5,12 @@ import numpy as np
 # Input layer with 784 input neurons
 # 128 neuron hidden layer
 # Output layer with 10 output neurons
-
 def initialize_weights_biases(hidden_layer_nodes: int):
     #Initialize the weights not as 0 to allow for change within the network
-    W1 = np.random.randn(784, hidden_layer_nodes) * np.sqrt(2.0 / 784)
+    #Use 5408 and not 784 because W1 wont interact with the original
+    #pixel values but the the flatten_output of the convolutional
+    #layer which is of shape (60000, 5408)
+    W1 = np.random.randn(5408, hidden_layer_nodes) * np.sqrt(2.0 / 5408)
     W2 = np.random.randn(hidden_layer_nodes, hidden_layer_nodes) * np.sqrt(2.0 / hidden_layer_nodes)
     W3 = np.random.randn(hidden_layer_nodes, 10) * np.sqrt(2.0 / hidden_layer_nodes)
 
@@ -26,58 +28,6 @@ def initialize_cnn_filters(num_filters: int):
     b_conv = np.zeros((num_filters, 1))
 
     return F1, b_conv
-
-def loss_derivatives(A3: np.ndarray, Y_train: np.ndarray) -> np.ndarray:
-    # The output error dZ3
-    dZ3 = A3 - Y_train
-    return dZ3
-
-def output_layer_derivatives(A2: np.ndarray, dZ3: np.ndarray):
-    # The output Weights Gradient dW2
-    m = A2.shape[0]
-
-    dW3 = (1 / m) * (A2.T @ dZ3)
-
-    # The output Bias Gradient db2
-    db3 = (1 / m) * np.sum(dZ3, axis = 0, keepdims = True)
-
-    return dW3, db3
-
-def hidden_layer_derivatives(dZ2: np.ndarray, W2: np.ndarray, Z1: np.ndarray, X_train: np.ndarray):
-    m = dZ2.shape[0]
-
-    # The output error after ReLU
-    dA1 = dZ2 @ W2.T
-
-    # The output error before ReLU
-    relu_derivative = (Z1 > 0)
-    dZ1 = dA1 * relu_derivative
-
-    # The output Weights Gradient W1
-    dW1 = (1 / m) * (X_train.T @ dZ1)
-
-    # The output Bias Gradient db1
-    db1 = (1 / m) * (np.sum(dZ1, axis = 0, keepdims = True))
-
-    return dW1, db1
-
-def second_hidden_layer_derivatives(dZ3: np.ndarray, Z2: np.ndarray, A1: np.ndarray, W3: np.ndarray):
-    m = A1.shape[0]
-
-    # The output error after ReLU
-    dA2 = dZ3 @ W3.T
-
-    # The output error before ReLU
-    relu_derivative = (Z2 > 0)
-    dZ2 = dA2 * relu_derivative
-
-    # The outpt Weights Gradient 2
-    dW2 = (1 / m) * (A1.T @ dZ2)
-    
-    # The output Bias Gradient db2
-    db2 = (1 / m) * (np.sum(dZ2, axis = 0, keepdims = True))
-
-    return dZ2, dW2, db2
 
 
 
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import numpy as np
 from data import load_and_prep_data
-from network import initialize_weights_biases, loss_derivatives, output_layer_derivatives, hidden_layer_derivatives, second_hidden_layer_derivatives
+from network import initialize_weights_biases, initialize_cnn_filters
+from forward import conv_forward, linear_forward, relu_forward, flatten_forward, softmax_forward
+from backpropagation import backpropagation, backpropagation_softmax
 from activations import ReLU, Softmax
 from losses import Categorical_Cross_Entropy
 from optimizers import gradient_descent
@@ -13,6 +15,7 @@ X_train, Y_train = load_and_prep_data(TRAIN_FILEPATH)
 Y_raw = np.argmax(Y_train, axis = 1)
 
 W1, W2, W3, b1, b2, b3 = initialize_weights_biases(512)
+F1, b_conv = initialize_cnn_filters(8)
 
 # Data to monitor training
 loss_history = []
@@ -20,33 +23,36 @@ accuracy_history = []
 
 print("Training loop started")
 for epoch in range(1000):
-    # First hidden layer
-    Z1 = X_train @ W1 + b1
-    A1 = ReLU(Z1)
+    # Pass the data through the convolutional layer
+    conv_output_data, conv_cache = conv_forward(X_train, F1, b_conv)
+    flattened_data, flatten_cache = flatten_forward(conv_output_data)
 
-    # Second hidden layer
-    Z2 = A1 @ W2 + b2
-    A2 = ReLU(Z2)
+    # Pass through first hidden layer
+    dense1_output, dense1_cache = relu_forward(flattened_data, W1, b1)
 
-    #Output layer
-    Z3 = A2 @ W3 + b3
-    A3 = Softmax(Z3)
+    # Pass through second hidden layer
+    dense2_output, dense2_cache = relu_forward(dense1_output, W2, b2)
+
+    # Output layer (Predictions)
+    dense3_output, dense3_cache = linear_forward(dense2_output, W3, b3)
+    predictions, softmax_cache = softmax_forward(dense3_output)
 
     # Accuracy Prediction
-    final_prediction = np.argmax(A3, axis = 1)
+    final_prediction = np.argmax(predictions, axis = 1)
     boolean_predictions = final_prediction == Y_raw
     accuracy = np.mean(boolean_predictions) * 100
     accuracy_history.append(accuracy)
 
     # Calculate loss
-    CCE_loss = Categorical_Cross_Entropy(A3, Y_train)
+    CCE_loss = Categorical_Cross_Entropy(predictions, Y_train)
     loss_history.append(CCE_loss)
 
     # Calculate derivatives with Chain Rule
-    dZ3 = loss_derivatives(A3, Y_train)
-    dW3, db3 = output_layer_derivatives(A2, dZ3)
-    dZ2, dW2, db2 = second_hidden_layer_derivatives(dZ3, Z2, A1, W3)
-    dW1, db1 = hidden_layer_derivatives(dZ2, W2, Z1, X_train)
+    #TODO make them with the new functions
+    # dZ3 = loss_derivatives(A3, Y_train)
+    # dW3, db3 = output_layer_derivatives(A2, dZ3)
+    # dZ2, dW2, db2 = second_hidden_layer_derivatives(dZ3, Z2, A1, W3)
+    # dW1, db1 = hidden_layer_derivatives(dZ2, W2, Z1, X_train)
 
     # Send to optimizer to apply gradient descent
     lr = 0.01
