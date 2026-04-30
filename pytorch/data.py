906fd29517d1fa324b3a25e2f0e424dd0953a7a3
Kawasaki <remvosp@gmail.com>
2026-04-30 18:32:42 +0300

    started writing the pytorch network

---
+++ pytorch/data.py
@@ -0,0 +1,46 @@
+import cupy as np
+
+def load_and_prep_data(filepath):
+    data = np.loadtxt(filepath, delimiter=',', skiprows=1)
+
+    # First column (labels)
+    Y_raw = data[:, 0]
+
+    # Pixel data (28x28 = 784 values)
+    X_raw = data[:, 1:]
+
+    # Normalize values so they are between 0.0 and 1.0
+    # Neural networks work faster with smaller numbers
+    X = X_raw / 255.0
+
+    m = X.shape[0]
+
+    X_train = X.reshape(m, 1, 28, 28)
+
+    # Create an array of zeros (number of images x 10)
+    num_images = Y_raw.shape[0]
+    Y_encoded = np.zeros((num_images, 10))
+
+    Y_encoded[np.arange(num_images), Y_raw.astype(int)] = 1.0
+
+    return X_train, Y_encoded
+
+def augment_data(data):
+    shift_y = np.random.randint(-2, 3)
+    shift_x = np.random.randint(-2, 3)
+
+    shifted_data = np.zeros_like(data)
+
+    dest_y1 = max(0, shift_y)
+    dest_y2 = min(28, 28 + shift_y)
+    dest_x1 = max(0, shift_x)
+    dest_x2 = min(28, 28 + shift_x)
+
+    src_y1 = max(0, -shift_y)
+    src_y2 = min(28, 28 - shift_y)
+    src_x1 = max(0, -shift_x)
+    src_x2 = min(28, 28 - shift_x)
+
+    shifted_data[:, :, dest_y1:dest_y2, dest_x1:dest_x2] = data[:, :, src_y1:src_y2, src_x1:src_x2]
+
+    return shifted_data
---
+++ pytorch/network.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+
+class MNIST(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Convolutional layer
+        # 1 color channel
+        # 8 filters
+        # 3x3 filter size
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding=0)
+
+        # ReLU
+        self.relu = nn.ReLU()
+
+        # Max Pool layer
+        # 2x2 block size
+        # Stride 2 (match block size)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Dense Layers
+        # Flatten Max Pool output
+        self.flatten = nn.Flatten()
+
+        # Hidden Layer 1
+        self.dense1 = nn.Linear(in_features=1352, out_features=128)
+
+        # Dropout Regularization
+        self.dropout = nn.Droupout(p=0.2)
+
+        # Hidden Layer 2
+        self.dense2 = nn.Linear(in_features=128, out_features=10)
+    
+    def forward(self, x):
+        # Forward Pass function
+        # Pytorch automatically stores cache
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.flatten(x)
+
+        x = self.dense1(x)
+        x = self.relu(x)
+        x = self.droput(x)
+
+        logits = self.dense2(x)
+
+        return logits
