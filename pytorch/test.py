d178d63787e2eb791181495785eba84ec6a0800d
Kawasaki <remvosp@gmail.com>
2026-05-08 16:19:11 +0300

    commented pytorch implementation

--- pytorch/test.py
+++ pytorch/test.py
@@ -7,40 +7,46 @@ TEST_FILEPATH = "mnist_test/mnist_test.csv"
 
 X_test, Y_test = load_and_prep_data(TEST_FILEPATH)
 
+# Convert numpy arrays into pytorch tensors
 X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
 Y_test_tensor = torch.tensor(Y_test, dtype=torch.float32)
 
+# Pick cuda as the device if available and send the tensors there
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 X_test_tensor = X_test_tensor.to(device)
 Y_test_tensor = Y_test_tensor.to(device)
 
+# TensorDataset combines the arrays so that X[0] is always paired with Y[0]
 test_dataset = TensorDataset(X_test_tensor, Y_test_tensor)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+# DataLoader splits the data into batches of 64. Shuffle is false in test cause
+# order doesn't matter in testing
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
+# Initialize model and load weights
 model = MNIST().to(device)
-
 model.load_state_dict(torch.load("trained_model.pth", weights_only=True))
-
+# Put model in eval mode (testing mode) so it doesnt keep track of gradients
 model.eval()
 
 running_correct_predictions = 0
 total_samples = 0
 
+# Test forward pass
 with torch.no_grad():
     for X_batch, Y_batch in test_loader:
-
-        X_batch_np = X_batch.cpu().numpy()
-
-        X_batch_tensor = torch.tensor(X_batch, dtype=torch.float32).to(device)
+        # Send the tensors to device because TensorDataset creates them in cpu
+        X_batch = X_batch.to(device)
         Y_batch = Y_batch.to(device)
 
         logits = model(X_batch)
 
+        # Labels and logits are in this format: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0] so we crush them into [2]
+        # to be easier to compare them
         Y_batch_indices = torch.argmax(Y_batch, dim=1)
-
         predictions = torch.argmax(logits, dim=1)
         running_correct_predictions += (predictions == Y_batch_indices).sum().item()
-        total_samples += X_batch_tensor.size(0)
+
+        total_samples += X_batch.size(0)
 
 epoch_accuracy = (running_correct_predictions / total_samples) * 100
 print(f"Testing complete. Accuracy: {epoch_accuracy:.2f}%")
--- pytorch/train.py
+++ pytorch/train.py
@@ -8,21 +8,29 @@ TRAIN_FILEPATH = "mnist_train/mnist_train.csv"
 
 X_train, Y_train = load_and_prep_data(TRAIN_FILEPATH)
 
+# Convert numpy arrays into pytorch tensors
 X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
 Y_train_tensor = torch.tensor(Y_train, dtype=torch.float32)
 
+# Pick cuda as the device if available and send the tensors there
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 X_train_tensor = X_train_tensor.to(device)
 Y_train_tensor = Y_train_tensor.to(device)
 
+# TensorDataset combines the arrays so that X[0] is always paired with Y[0]
 train_dataset = TensorDataset(X_train_tensor, Y_train_tensor)
+# DataLoader splits the data into batches of 64. Shuffle is true in training cause
+# order does matter because the network may memorize the order
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
+# Initialize model
 model = MNIST().to(device)
 
+# Initialize loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
+# Training loop
 for epoch in range(30):
     running_loss = 0.0
     running_correct_predictions = 0
@@ -30,20 +38,23 @@ for epoch in range(30):
 
     for X_batch, Y_batch in train_loader:
 
-        X_batch_tensor = X_batch.to(device)
-        X_batch_augmented = augment_data(X_batch_tensor)
-
-        Y_batch = Y_batch.to(device)
-        Y_batch_indices = torch.argmax(Y_batch, dim=1)
+        X_batch = X_batch.to(device)
+        X_batch_augmented = augment_data(X_batch)
 
         logits = model(X_batch_augmented)
         loss = criterion(logits, Y_batch)
 
+        # Labels and logits are in this format: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0] so we crush them into [2]
+        # to be easier to compare them
+        Y_batch = Y_batch.to(device)
+        Y_batch_indices = torch.argmax(Y_batch, dim=1)
         predictions = torch.argmax(logits, dim=1)
         running_correct_predictions += (predictions == Y_batch_indices).sum().item()
-        running_loss += loss.item() * X_batch_tensor.size(0)
-        total_samples += X_batch_tensor.size(0)
 
+        running_loss += loss.item() * X_batch.size(0)
+        total_samples += X_batch.size(0)
+
+        # Reset optimizer, calculate gradients with backpropagation and change them
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -53,4 +64,5 @@ for epoch in range(30):
         epoch_accuracy = (running_correct_predictions / total_samples) * 100
         print(f"Epoch: {epoch + 1}, Loss: {epoch_loss:.2f}, Accuracy: {epoch_accuracy:.2f}%")
     
+# Save model weights
 torch.save(model.state_dict(), "trained_model.pth")
